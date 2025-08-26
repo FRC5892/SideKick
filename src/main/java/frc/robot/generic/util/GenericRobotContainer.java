80d6228f46617bde632deaf18c1d224130cb07fb
Michael <117385299+mstatistics@users.noreply.github.com>
2025-08-26 06:39:43 -0500

    Stuff

---
+++ src/main/java/frc/robot/generic/util/GenericRobotContainer.java
@@ -0,0 +1,16 @@
+package frc.robot.generic.util;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public interface GenericRobotContainer {
+    /** Returns the Autonomous Command. This command is automatically scheduled when Autonomous Starts */
+    Command getAutonomousCommand();
+
+    /** This function is called periodically during all modes. */
+    default void robotPeriodic() {}
+    /** This function is called once when the robot is first started up. */
+    default void simulationInit() {}
+
+    /** This function is called periodically whilst in simulation. */
+    default void simulationPeriodic() {}
+}
